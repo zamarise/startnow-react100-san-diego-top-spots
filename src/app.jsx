37e4dca8d91edb33272b5735c46fda2079b59ce6
Zamarise Montoya <zamarise@gmail.com>
2017-11-27 20:01:08 -0800

    Add code for initial functionality

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
   render() {
     return (
-      <div className='App'></div>
+      <div className='container-fluid'>
+        <h1 className='text-center'>San Diego Top Spots</h1>
+        <p className='lead bg-light text-gray-dark text-center'>
+          A list of the top 30 places to see in San Diego, California (made in React).
+        </p>
+        <div className='App' />
+      </div>
     );
   }
 }
