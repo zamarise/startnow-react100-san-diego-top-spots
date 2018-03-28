730f8681c1fceae2bea1fec6f70fcb37f974fa12
Zamarise Montoya <zamarise@gmail.com>
2018-03-27 19:52:32 -0700

    Add updates

--- src/app.jsx
+++ src/app.jsx
@@ -4,11 +4,19 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = { topspots: [] };
+  }
+
+  componentWillMount() {
+    axios
+      .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+      .then(response => response.data)
+      .then(topspots => this.setState({ topspots }));
   }
   render() {
     return (
       <div className='container-fluid'>
+        <pre>{JSON.stringify(this.state.topspots, null, 2)}</pre>
         <h1 className='text-center'>San Diego Top Spots</h1>
         <p className='lead bg-light text-gray-dark text-center'>
           A list of the top 30 places to see in San Diego, California (made in React).
