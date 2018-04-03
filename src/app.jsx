ad348b6ee000c0c45bdeef343821a753f894af52
Zamarise Montoya <zamarise@gmail.com>
2018-04-02 19:13:45 -0700

    Update code

--- src/app.jsx
+++ src/app.jsx
@@ -17,11 +17,11 @@ class App extends Component {
   render() {
     return (
       <div className='container-fluid'>
-        <pre>{JSON.stringify(this.state.topspots, null, 2)}</pre>
         <h1 className='text-center'>San Diego Top Spots</h1>
         <p className='lead bg-light text-gray-dark text-center'>
           A list of the top 30 places to see in San Diego, California (made in React).
         </p>
+        <pre>{JSON.stringify(this.state.topspots, null, 2)}</pre>
         <div className='App' />
       </div>
     );
