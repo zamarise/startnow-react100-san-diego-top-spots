81409019f580a76cae6a9f9f321d91cc3573b3f0
Zamarise Montoya <zamarise@gmail.com>
2018-04-03 19:12:24 -0700

    Add buttons to Google Map links

--- src/topspot.jsx
+++ src/topspot.jsx
@@ -5,7 +5,9 @@ export default props => (
     <h4>{props.name}</h4>
     <p>{props.description}</p>
     <a href={ `https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=${props.location}` }>
-      Open in Google Maps
+      <button type='button' className='btn btn-primary btn-lg btn-block'>
+        Open in Google Maps
+      </button>
     </a>
   </div>
 );
